98a9a6fcafaccb93b118832096d5e0883675dc1a
asoutherncat <lysunny0302@163.com>
2024-04-04 01:13:04 +0800

    update:camera initial pos

--- src/components/js/base/camera.js
+++ src/components/js/base/camera.js
@@ -2,7 +2,7 @@ import { PerspectiveCamera } from "three";
 import { sizes } from "../system/sizes";
 function createCamera(){
     const camera = new PerspectiveCamera(65, sizes.width / sizes.height, 1, 2000)
-    camera.position.set(0, 7, 6)
+    camera.position.set(0, 4, 6.5)
     return camera
 }
 export {createCamera }
