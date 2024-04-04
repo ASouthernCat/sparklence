195155b235a32a5c7a74666116872c27a3627cd7
asoutherncat <lysunny0302@163.com>
2024-04-04 10:36:27 +0800

    update:scene.background

--- src/components/js/base/scene.js
+++ src/components/js/base/scene.js
@@ -4,7 +4,7 @@ import * as TEXTURE from '../texture/index.js'
 function createScene() {
   const scene = new Scene();
 
-  // scene.background = new Color('skyblue');
+  scene.background = new Color(0x000000);
   // scene.background = TEXTURE.skyTextureEquirec;
   // scene.backgroundIntensity = 0.1
   scene.environment = TEXTURE.envMap;
